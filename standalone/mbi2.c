b0f0782f7b17f77723779891a6343b919e44c823
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2018-08-07 11:44:56 +0200

    mbi2: more overlaps checks

--- standalone/mbi2.c
+++ standalone/mbi2.c
@@ -26,22 +26,6 @@ static
 uint64_t find_mbi2_memory(void * multiboot, size_t const binary_size,
                           bool highest, uint64_t mem_below)
 {
-  struct mbi2_fb * fb = 0;
-
-  for (struct mbi2_tag *i = mbi2_first(multiboot); i; i = mbi2_next(i)) {
-    if (i->type != MBI2_TAG_FB)
-      continue;
-
-    fb = (struct mbi2_fb *) (i + 1);
-
-    printf("framebuffer at [%llx+%llx) %ux%u@%u\n", fb->addr,
-           fb->addr + fb->pitch * fb->height, fb->width, fb->height, fb->bpp);
-    if (fb->type != 1)
-      printf("warning - unknown framebuffer type\n");
-
-    break;
-  }
-
   uint64_t binary_start = 0;
 
   for (struct mbi2_tag *i = mbi2_first(multiboot); i; i = mbi2_next(i)) {
@@ -72,10 +56,24 @@ uint64_t find_mbi2_memory(void * multiboot, size_t const binary_size,
 
       /* exclude bender image */
       exclude_bender_binary(&mem_start, &mem_size);
-      /* exclude framebuffer */
-      if (fb && mem_size)
-        exclude_region(&mem_start, &mem_size, fb->addr,
-                       fb->addr + fb->pitch * fb->height - 1);
+
+      /* exclude multiboot struct */
+      exclude_region(&mem_start, &mem_size, (uintptr_t)multiboot,
+                     (uintptr_t)multiboot + mbi2_size(multiboot) - 1);
+
+      /* exclude region in use */
+      for (struct mbi2_tag *i = mbi2_first(multiboot); i; i = mbi2_next(i)) {
+        if (i->type == MBI2_TAG_FB) {
+          struct mbi2_fb const * const fb = (struct mbi2_fb * const) (i + 1);
+          exclude_region(&mem_start, &mem_size, fb->addr,
+                         fb->addr + fb->pitch * fb->height - 1);
+        } else
+        if (i->type == MBI2_TAG_MODULE) {
+          struct mbi2_module * module = (struct mbi2_module *)(i + 1);
+          exclude_region(&mem_start, &mem_size, module->mod_start,
+                         module->mod_end);
+        }
+      }
 
       if (!(m->type == MMAP_AVAIL && binary_size <= mem_size))
         continue;
@@ -94,21 +92,22 @@ uint64_t find_mbi2_memory(void * multiboot, size_t const binary_size,
   return binary_start;
 }
 
+enum Check_mem { NO_ERROR = 0, ERROR_MBI2 = 1, ERROR_NA = 2 };
 
 static
 int check_mem (struct ph64 const * p, uint32_t const binary, uint8_t ** var)
 {
   if (p->p_type != ELF_PT_LOAD)
-    return 0;
+    return NO_ERROR;
 
   void * multiboot = (void *)var;
   uintptr_t mbi2_addr = (uintptr_t)multiboot;
   uint32_t mbi2_len = mbi2_size(multiboot);
 
   if (in_range(p, mbi2_addr, mbi2_len)) {
-    printf("Multiboot struct %llx+%llx overlaps with phdr %llx+%llx\n",
+    printf("Multiboot struct %lx+%lx overlaps with phdr %llx+%llx\n",
            mbi2_addr, mbi2_len, p->p_paddr, p->p_memsz);
-    return 1;
+    return ERROR_MBI2;
   }
 
   bool in_ram = false;
@@ -137,20 +136,20 @@ int check_mem (struct ph64 const * p, uint32_t const binary, uint8_t ** var)
       if (in_range(p, m->addr, m->len)) {
         printf("Reserved memory %llx+%llx type=%u overlaps with phdr %llx+%llx\n",
                m->addr, m->len, m->type, p->p_paddr, p->p_memsz);
-        return 1;
+        return ERROR_NA;
       }
     }
   }
 
   /* check that p does not overlap with bender binary */
   if (overlap_bender_binary(p))
-    return 1;
+    return ERROR_NA;
 
   if (in_ram)
-    return 0;
+    return NO_ERROR;
 
   printf("phdr %llx+%llx is outside RAM !\n", p->p_paddr, p->p_memsz);
-  return 2;
+  return ERROR_NA;
 }
 
 static
@@ -182,6 +181,8 @@ start_module2(void *multiboot, bool uncompress, uint64_t phys_max)
   struct mbi2_module * modules[32];
   unsigned module_count = 0;
 
+  retry:
+
   /* collect modules and module count and invalidate current CMD_LINE */
   for (struct mbi2_tag *i = mbi2_first(multiboot); i; i = mbi2_next(i)) {
     if (i->type == MBI2_TAG_MODULE) {
@@ -208,17 +209,30 @@ start_module2(void *multiboot, bool uncompress, uint64_t phys_max)
   }
   modules[module_count] = 0;
 
-  /* sanity check that next module will be unpacked to free memory */
-  int error = for_each_phdr(modules[0]->mod_start, (uint8_t **)multiboot, check_mem);
-  if (error)
-    return error;
-
-  /**
-   * Relocate all modules to high memory. Must be below 4G since mod_start is
-   * solely 32bit value !
-   */
+  /* relocation must be below 4G since mod_start is solely 32bit value */
   uint64_t mem_below = (phys_max > 1ULL << 32) ? 1ULL << 32 : phys_max;
 
+  /* sanity check that next module will be unpacked to free memory */
+  int error = for_each_phdr(modules[0]->mod_start, (uint8_t **)multiboot, check_mem);
+  if (error != NO_ERROR) {
+    if (error != ERROR_MBI2)
+      return error;
+
+    /* relocate the multiboot structure */
+    uint64_t relocate_addr = find_mbi2_memory(multiboot, mbi2_size(multiboot), true, mem_below);
+    if (!relocate_addr)
+      return error;
+
+    mem_below = relocate_addr;
+    memcpy((char *)(uintptr_t)relocate_addr, (void *)multiboot, mbi2_size(multiboot));
+    printf("relocate multiboot struct %p -> %p\n", multiboot, relocate_addr);
+    multiboot = (void *)(uintptr_t)relocate_addr;
+
+    /* modules contain pointer to old multiboot, re-start evaluation */
+    goto retry;
+  }
+
+  /* relocate modules if required */
   int i = 0;
   while ((i = for_each_phdr(modules[0]->mod_start, (uint8_t **)modules, check_reloc))) {
     struct mbi2_module * rel_module = modules[i - 1];
